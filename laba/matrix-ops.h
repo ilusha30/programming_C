dca18381633f1bf4625111317cef818b780aad02
Zyrex <forzyrex@gmail.com>
2025-05-12 14:32:14 +1000

    Update matrix-ops.h

--- laba/matrix-ops.h
+++ laba/matrix-ops.h
@@ -8,6 +8,23 @@
 #ifndef MATRIX_OPS_H
 #define MATRIX_OPS_H
 
-double* matrix_operation(const double* a, const double* b, int n, char op);
+// Выделение памяти под матрицу n x n
+double** create_matrix(int n);
+
+// Освобождение памяти матрицы n x n
+void free_matrix(double** matrix, int n);
+
+// Ввод элементов матрицы n x n с консоли
+void input_matrix(double** matrix, int n);
+
+// Вывод матрицы n x n на консоль
+void print_matrix(double** matrix, int n);
+
+
+double** matrix_plus(double**a ,double** b, int n);
+double**  matrix_min(double**a ,double** b, int n);
+double**  matrix_um(double**a ,double** b, int n);
+double** matrix_operation(double** a, double** b, int n, char op);
 
 #endif // MATRIX_OPS_H
+
