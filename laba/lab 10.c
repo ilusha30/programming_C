498a92c3efcf9ce560239ba78f961ea8ad7b3169
Zyrex <forzyrex@gmail.com>
2025-05-12 15:06:01 +1000

    Update lab 10.c

--- laba/lab 10.c
+++ laba/lab 10.c
@@ -1,21 +1,24 @@
-//
-//  lab 10.c
-//  C labs
-//
-//  Created by Илья Овсянников on 10.05.2025.
-//Написать программу вычисления максимального числа из введенных пользователем в виде одной строки.  Использовать для этого макрос (без функции), на вход которому по-дается массив чисел и их количество.
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define MAX_NUMBERS 100
+
 #define MAX_IN_ARRAY(arr, n, max) \
     do { \
         max = arr[0]; \
-        for (int i = 1; i < n; i++) { \
+        for (int i = 1; i < (n); i++) { \
             if (arr[i] > max) max = arr[i]; \
         } \
+    } while(0)
+
+#define AVERAGE(arr, n, avg) \
+    do { \
+        int sum = 0; \
+        for (int i = 0; i < (n); i++) { \
+            sum += (arr)[i]; \
+        } \
+        (avg) = ((n) > 0) ? ((double)sum / (n)) : 0.0; \
     } while(0)
 
 int main(void) {
@@ -29,25 +32,19 @@ int main(void) {
         return 1;
     }
 
-
-    // Разбиваем строку на числа
     char *token = strtok(line, " ");
     while (token != NULL && count < MAX_NUMBERS) {
         numbers[count++] = atoi(token);
         token = strtok(NULL, " ");
     }
 
+
     int max_value;
     MAX_IN_ARRAY(numbers, count, max_value);
 
-    int sum = 0;
-    for (int i = 0; i < count; i++) {
-        sum += numbers[i];
-    }
+    double average;
+    AVERAGE(numbers, count, average);
 
-    //среднее арифметическое
-    double average = (double)sum / count;
-    
     printf("Максимальное число: %d\n", max_value);
     printf("Среднее арифметическое: %.2f\n", average);
 
